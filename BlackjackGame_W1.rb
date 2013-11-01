76e7d0f5375c4a72a5c467cf7cdb1a750366c7ea
crokobit <crokobit@gmail.com>
2013-11-01 15:49:40 +0800

    BJ

---
+++ BlackjackGame_W1.rb
@@ -0,0 +1,90 @@
+Card = Struct.new(:suit, :value ,:show_value)
+
+#bad implementation
+def determine_show_value value
+  show_value = 0
+  if value == 1
+    show_value = 'A'
+  elsif value == 11
+    show_value = 'J'
+  elsif value == 12
+    show_value = 'Q'
+  elsif value == 13
+    show_value = 'K'
+  else
+    show_value = value
+  end
+  show_value
+end
+
+class Deck
+  def initialize how_many_decks
+    @deck = []
+    suits_array = ['♠','♥','♣','♦']
+    how_many_decks.times do
+      suits_array.each do |suit|
+        counter = 1
+        while counter <=13
+          card = Card.new(suit,counter,determine_show_value(counter))
+          counter += 1
+          @deck.push card
+        end
+      end
+    end
+  end
+
+  def puts_all_cards
+    puts @deck
+  end
+
+  def shuffle
+    @deck.shuffle!
+  end
+
+  def pop
+    @deck.pop
+  end
+end
+
+class People
+  def initialize name
+    @name = name
+    @cards = []
+    @total_value = 0
+    @dealer = false
+  end
+  def is_dealer
+    @dealer = true
+  end
+  def status
+    if @total_value > 21
+      'busted'
+    elsif @total_value < 21
+      'safe'
+    elsif @total_value == 21
+      'BlackJack'
+    else
+      puts 'ERROR!! HERE'
+  end
+
+  def get_card card
+    @cards.pop = card
+    #@total_value += card[:value]
+  end
+
+  def puts_cards
+    puts"#{@name} has cards"
+    puts @cards
+    puts"total value = #{@total_value}"
+  end
+
+end
+
+deck = Deck.new(3)
+deck.shuffle
+#deck1.puts_all_cards
+#puts deck1.pop_card
+people1 = People.new('Ivan')
+people1.puts_cards
+people.get_card(deck.pop)
+people1.puts_cards
---
+++ test.rb
@@ -0,0 +1,8 @@
+#i = gets.chomp
+i = 2
+case i
+when  1..3
+  puts 'XX'
+when  3..6
+  puts 'GG'
+end
